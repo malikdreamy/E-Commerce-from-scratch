1c631c0e02d0470fbef8d529531e8ca380f2a72a
Malik <119153047+malikdreamy@users.noreply.github.com>
2024-01-20 01:25:29 -0800

    Update about-us.js

--- server/public/about-us.js
+++ server/public/about-us.js
@@ -4,7 +4,7 @@ const checkOutBtn = document.querySelector(".check-out-cart");
 
 const getVisitor = async () =>{
 
-    const visitor = await fetch('https://ipinfo.io/json?token=1ceaf0e8df9886');
+  const visitor = await fetch('https://ipinfo.io/json?token=');
   const visitorInfo = await visitor.json()
   // console.log(visitorInfo);
      let ip = visitorInfo.ip;
